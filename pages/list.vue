290ac2635175204b8e3a2f3fa091a6c832d08990
beckspark <stephenpbeck@gmail.com>
2025-07-19 07:48:29 -0600

    Update list language

--- pages/list.vue
+++ pages/list.vue
@@ -4,86 +4,8 @@
 <template>
     <div class="desc" style="line-height: 2;">
         <h1>Mt. Pleasant Clubs & Community Organizations</h1>
-        <p>Interested in joining a local club? Feel free to browse the alphabetical list of local clubs and organizations hosted right here in Mount Pleasant.
-            Know of a club that’s missing? Reach out and we’ll be sure to add it to the list below!</p>
-        <h2>Amigos Park Coalition</h2>
-        <ul>
-            <p>The Amigos Park project will transform an outdated site into a vibrant public park.  The aim is to revitalize the existing site location and create a welcoming community space.
-                This project will undertake new site enhancements to improve the park’s overall appeal and functionality</p>
-        </ul>
-        <h2><a href="https://anc1d.org">ANC1D</a></h2>
-        <ul>
-            <p>Advisory Neighborhood Commissions (ANCs) are locally elected bodies which consider a wide range of policies and programs affecting their neighborhoods,
-                including traffic, parking, recreation, street improvements, liquor licenses, zoning, economic development, police protection, sanitation and trash collection, and the District's annual budget.</p>
-        </ul>
-        <h2><a href="https://www.brownstreetmakercoop.org/">Brown Street Maker Cooperative</a></h2>
-        <ul>
-            <p>The Brown Street Maker Cooperative is a non-profit organization dedicated to providing its members with the space,
-                materials, and tools to build a wide variety of crafts and to engage in sustainability projects.</p>
-        </ul>
-        <h2><a href="">City Garden Co-op</a></h2>
-        <ul>
-            <p>City Garden is a member owned and operated coop in Mt. Pleasant, Washington, DC. Produce is purchased from local farmers markets and all food is priced at cost. A small markup is then added at the register to cover the costs of running the coop.</p>
-        </ul>
-        <h2><a href="https://freedcproject.org/">Free DC - Mt. Pleasant Chapter</a></h2>
-        <ul>
-            <p></p>
-        </ul>
-        <h2><a href="https://lostorigins.gallery">Lost Origins Gallery</a></h2>
-        <ul>
-            <p>Lost Origins is a privately-owned arts and culture space located on historic Mount Pleasant Street in Washington D.C. Founded by Jason Hamacher, DC musician, photographer and documentarian, Lost Origins grew out of Hamacher’s extensive experience in the arts, culture, and diplomatic communities through his company, Lost Origin Productions. Lost Origins strives to be inviting, engaging and intimate. We host exhibitions and events by local, national and international artists, comedians, filmmakers, musicians and authors. We believe each exhibit and event is a collaborative effort, and we are strengthened by the support of our staff, neighbors, and friends.</p>
-        </ul>
-        <h2><a href="">Marx at The Movies</a></h2>
-        <ul>
-            <p>Weekly backyard movies series in Mt Pleasant showing screenings of labor related films with neighbors. Snacks, vibes, and seats are all provided and stay for a catchup and conversation afterwards</p>
-        </ul>
-        <h2><a href="https://www.dclibraryfriends.org/mount-pleasant">Mt. Peasant Library Friends</a></h2>
-        <ul>
-            <p>Nonprofit that supports and advocates for DCPL. All fundraising goes towards supporting local programming at the Mt. Pleasant Library</p>
-        </ul>
-        <h2><a href="https://sites.google.com/view/mtp-dc-artists-collective/home">Mt. Pleasant Artists Collective</a></h2>
-        <ul>
-            <p>Collective of artists in the historic Mount Pleasant neighborhood of Washington DC</p>
-        </ul>
-        <h2><a href="https://www.instagram.com/mtpfitnessclub/">Mt. Pleasant Fitness Club</a></h2>
-        <ul>
-            <p>Outdoor exercise classes for everybody-- feel-good movement, friendship, fun, and fresh air!📍Bancroft E.S. Field</p>
-        </ul>
-        <h2><a href="https://www.instagram.com/mtplantry/">Mt. Plantry</a></h2>
-        <ul>
-        </ul>
-        <h2><a href="">Mt. Pleasant Tool Library</a></h2>
-        <ul>
-            <p></p>
-        </ul>
-        <h2><a href="https://mountpleasantvillage.org/">Mt. Pleasant Village</a></h2>
-        <ul>
-            <p>Mount Pleasant Village is a nonprofit, all-volunteer membership organization that enables members to celebrate the opportunities and meet the challenges of aging in the community.</p>
-        </ul>
-        <h2><a href="https://www.instagram.com/scoobydoomansion/">Scooby Doo Mansion</a></h2>
-        <ul>
-            <p>An old house in Mt Pleasant, DC welcoming friends and guests for live music and rich community🍉</p>
-        </ul>
-        <h2><a href="">Share a Seed</a></h2>
-        <ul>
-            <p>Share a Seed is a mutual aid seed sharing and growing program founded by Slow Food DC Board Member, Reana Kim at the beginning of the coronavirus pandemic. Share a Seed replaces scarcity with abundance by leveraging the power of community to fill resource gaps. Growers and gardeners share their spares with the project (seeds, garden equipment, soil), which are then redistributed throughout the the community. Share a Seed seeks to connect under-resourced community members to the resources, mentorship, and tools they need to get growing!</p>
-        </ul>
-        <h2><a href="https://www.instagram.com/sonlalucha/">Son LA Lucha Workshop</a></h2>
-        <ul>
-            <p>Spanish Language Immersion Speaking Group</p>
-        </ul>
-        <h2><a href="">Spanish Language Immersion Speaking Group</a></h2>
-        <ul>
-            <p>This group meets bi-monthly at Dos Gringos to flex and practice their Spanish speaking skills with fellow neighbors</p>
-        </ul>
-        <h2><a href="https://linktr.ee/dcw1mutualaid">Ward 1 Mutual Aid</a></h2>
-        <ul>
-            <p>A grassroots, community-led effort organizing to take care of each other & build a new, liberated world. #WeKeepUsSafe. On occupied Piscataway land.</p>
-        </ul>
-        <h2><a href="https://www.instagram.com/woodnerunion/">Woodner Tenants Union</a></h2>
-        <ul>
-            <p>Organizing to prevent evictions in the largest apt building in DC! ¡Organizándonos para evitar los desalojos en el edificio de apts más grande de DC!</p>
-        </ul>
+        <p>Interest to get more involved? <a href="https://docs.google.com/spreadsheets/d/18r_jarTf2OR1ZeqovwPPEPsHAND9HyjuCTnGZi51ahQ">Check out Mt Pleasant's rich tapestry of organizations, collectives, and interest groups!</a></p>
+        <p>Know of some Mt. Pleasant magic that's missing from this list? Please reach out and let us know! <a href="mailto:mtpleasantcommunitycalendar@gmail.com">mtpleasantcommunitycalendar@gmail.com</a></p>
     </div>
     <Footer />
 </template>
